677d143fe31f18fc3a822b044835dec1d716df4c
Payal-mak <iampayal018@gmail.com>
2026-03-07 21:46:23 +0530

    chore: 404s use next(new AppError(…)) instead of inline render

--- crud_operations/routes/itemRoutes.js
+++ crud_operations/routes/itemRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/itemController');
+const { validateItem } = require('../middlewares/validate');
 
 // List all items
 router.get('/', itemController.listItems);
@@ -8,14 +9,14 @@ router.get('/', itemController.listItems);
 // Show add item form
 router.get('/add', itemController.showAddForm);
 
-// Create a new item
-router.post('/add', itemController.createItem);
+// Create a new item  (validate first)
+router.post('/add', validateItem, itemController.createItem);
 
 // Show edit item form (must be before /:id)
 router.get('/edit/:id', itemController.showEditForm);
 
-// Update an item
-router.post('/edit/:id', itemController.updateItem);
+// Update an item  (validate first)
+router.post('/edit/:id', validateItem, itemController.updateItem);
 
 // Delete an item
 router.post('/delete/:id', itemController.deleteItem);
