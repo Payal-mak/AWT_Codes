ab2c24231bec3447ba6c9e1adc1172017f9822fe
Payal-mak <iampayal018@gmail.com>
2026-03-07 21:47:52 +0530

    chore: validateClaim  middleware on POST add + edit

--- crud_operations/routes/claimRoutes.js
+++ crud_operations/routes/claimRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const claimController = require('../controllers/claimController');
+const { validateClaim } = require('../middlewares/validate');
 
 // List all claims
 router.get('/', claimController.listClaims);
@@ -8,14 +9,14 @@ router.get('/', claimController.listClaims);
 // Show add claim form
 router.get('/add', claimController.showAddForm);
 
-// Create a new claim
-router.post('/add', claimController.createClaim);
+// Create a new claim  (validate first)
+router.post('/add', validateClaim, claimController.createClaim);
 
 // Show edit claim form
 router.get('/edit/:id', claimController.showEditForm);
 
-// Update a claim
-router.post('/edit/:id', claimController.updateClaim);
+// Update a claim  (validate first)
+router.post('/edit/:id', validateClaim, claimController.updateClaim);
 
 // Delete a claim
 router.post('/delete/:id', claimController.deleteClaim);
